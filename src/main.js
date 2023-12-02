e89787f95910b8b67d65bdcf918a71fc32c25141
panfilov.v <panfilov.v@clickon.pro>
2023-12-02 16:31:28 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import bridge from "@vkontakte/vk-bridge";
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+bridge.send('VKWebAppInit');
