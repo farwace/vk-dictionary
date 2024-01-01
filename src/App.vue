19d93cc040fb6c90ecce54f72ea53d654b78f6ed
panfilov.v <panfilov.v@clickon.pro>
2024-01-01 16:33:20 +0300

    Дописал коммент насчет слайдов для инструкции

--- src/App.vue
+++ src/App.vue
@@ -75,7 +75,9 @@ watch(isReady, (neoVal) => {
   if(neoVal){
     if(user.value.isNew){
       UI?.showWelcomeSlides({
-        //TODO: СДЕЛАТЬ КРАСИВЫЕ СЛАЙДЫ И ОПИСАТЬ ЧТО ДЕЛАТЬ! https://dev.vk.com/ru/mini-apps/development/information-screens
+        /*TODO: СДЕЛАТЬ КРАСИВЫЕ СЛАЙДЫ И ОПИСАТЬ ЧТО ДЕЛАТЬ! https://dev.vk.com/ru/mini-apps/development/information-screens
+                Вынести эти слайды в json файлы, подгружать ассинхронно и вынести в отдельную функцию что ли. В окне настроек дать возможность посмотреть инструкцию еще раз...
+        * */
         slides: [
           {
             media: {
